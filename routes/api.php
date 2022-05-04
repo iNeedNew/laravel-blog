27507ef1e9a5de97c051008420818fa1dba2feac
iNeedNew <official.andrei.nagornyi@gmail.com>
2022-05-04 13:58:33 +0300

    access for auth

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,42 @@ use Illuminate\Support\Facades\Route;
 //});
 
 
-
-
 Route::group(['middleware' => 'api', 'prefix' => '/auth'], function ($router) {
     Route::post('/register', 'AuthController@register');
     Route::post('/login', 'AuthController@login');
-    Route::post('/logout',  'AuthController@logout');
+    Route::post('/logout', 'AuthController@logout');
     Route::post('/refresh', 'AuthController@refresh');
     Route::post('/me', 'AuthController@me');
+
+
+    Route::group(['middleware' => 'auth:api'], function () {
+
+
+        Route::group(['namespace' => 'Post', 'prefix' => '/posts'], function () {
+
+            Route::get('/', 'IndexController');
+            Route::get('/{post}', 'ShowController');
+
+            Route::post('/', 'StoreController');
+            Route::patch('/{post}', 'UpdateController');
+            Route::delete('/{post}', 'DestroyController');
+
+            Route::group(['namespace' => 'Comment', 'prefix' => '/{post}/comments'], function () {
+                Route::get('/', 'IndexController');
+                Route::post('/', 'StoreController');
+                Route::patch('/{comment}', 'UpdateController');
+                Route::delete('/{comment}', 'DestroyController');
+            });
+
+            Route::group(['namespace' => 'Reaction', 'prefix' => '/{post}'], function () {
+                Route::get('/reactions', 'IndexController');
+            });
+        });
+
+
+    });
+
+
 });
 
 
@@ -34,23 +62,4 @@ Route::group(['namespace' => 'User', 'prefix' => '/users'], function () {
     Route::post('/', 'StoreController');
 });
 
-Route::group(['namespace' => 'Post', 'prefix' => '/posts'], function () {
 
-    Route::get('/', 'IndexController');
-    Route::get('/{post}', 'ShowController');
-
-    Route::post('/', 'StoreController');
-    Route::patch('/{post}', 'UpdateController');
-    Route::delete('/{post}', 'DestroyController');
-
-    Route::group(['namespace' => 'Comment', 'prefix' => '/{post}/comments'], function () {
-        Route::get('/', 'IndexController');
-        Route::post('/', 'StoreController');
-        Route::patch('/{comment}', 'UpdateController');
-        Route::delete('/{comment}', 'DestroyController');
-    });
-
-    Route::group(['namespace' => 'Reaction', 'prefix' => '/{post}'], function () {
-        Route::get('/reactions', 'IndexController');
-    });
-});
